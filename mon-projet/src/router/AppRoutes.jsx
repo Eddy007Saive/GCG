21419b6fd58a9824f6cc0cb6aa2d8a576de4a50b
Eddy007 <laplumenoire001@gmail.com>
2025-01-27 15:59:35 +0300

    front vaovao

---
+++ mon-projet/src/router/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+import Create from '../pages/Employee/Create';
+
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} >
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRoutes;

0cc74edeb7c3f65555910d84f4f9d1bc20218ee9
Eddy007 <laplumenoire001@gmail.com>
2025-01-29 15:41:34 +0300

    style formulaire

--- mon-projet/src/pages/Employee/EmployeList.jsx
+++ mon-projet/src/pages/Employee/EmployeList.jsx
@@ -40,7 +40,12 @@ function EmployeList() {
     return (
       <div>
           <div className="w-full flex-start flex pb-5 items-center  ">
-            <Link to="create">Nouveau</Link>
+          <Link to="create" className="flex gap-2 bg-green-200 p-3 rounded-lg ">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+            </svg>
+            <p>Nouveau</p>
+          </Link>
           </div>
             <p>Aucun employé trouvé.</p>
       </div>
@@ -51,10 +56,15 @@ function EmployeList() {
     <div className="relative overflow-x-auto">
       <h1 className="text-lg pb-4 font-bold ">Liste des employées</h1>
       <div className="w-full flex-start flex pb-5 items-center  ">
-          <Link to="create">Nouveau</Link>
+          <Link to="create" className="flex gap-2 bg-green-200 p-3 rounded-lg ">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+            </svg>
+            <p>Nouveau</p>
+          </Link>
       </div>
-      <table className="w-full text-sm text-left text-gray-600">
-        <thead className="text-xs text-white uppercase bg-blue-600">
+      <table className="w-full text-sm text-left text-gray-600  ">
+        <thead className="text-xs text-white uppercase bg-blue-600 rounded-lg">
           <tr>
             <th scope="col" className="px-6 py-4">Matricule</th>
             <th scope="col" className="px-6 py-4">Nom</th>

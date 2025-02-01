0e6e4e5c20eb3645ae23f3d5b39b62e8aa8daeaa
EddySaive007 <laplumenoire001@gmail.com>
2025-01-31 18:01:07 -0600

    ddd

--- mon-projet/src/pages/Employee/EmployeDetails.jsx
+++ mon-projet/src/pages/Employee/EmployeDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { findEmploye } from '../../services/Employee';
 
 function EmployeDetails() {
@@ -96,16 +96,16 @@ function EmployeDetails() {
         </div>
       </div>
       <div className="w-full flex  flex-end gap-4  p-3  ">
-        <button className='bg-yellow-200 p-3 rounded-lg hover:bg-yellow-500 '>
+        <Link to={`/dashboard/employe/employe_update/${employe.id}`} className='bg-yellow-200 p-3 rounded-lg hover:bg-yellow-500 '>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
         </svg>
 
-        </button>
+        </Link>
         <button className='bg-red-200 p-3 rounded-lg hover:bg-red-500 '><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
               </svg>
-</button>
+        </button>
         <button className='bg-green-200 p-3 rounded-lg hover:bg-green-500 '>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z" />
--- mon-projet/src/pages/Employee/EmployeUpdate.jsx
+++ mon-projet/src/pages/Employee/EmployeUpdate.jsx
@@ -1,11 +1,320 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { createEmploye, findEmploye } from "../../services/Employee";
+import { useParams } from "react-router-dom";
 
-function Update() {
+function EmployeUpdate() {
+  const [employe,setEmploye]=useState(null)
+  const [loading,setLoaging]=useState(true)
+  const [error,setError]=useState(null)
+  const {id}=useParams()
+  useEffect(()=>{
+    console.log("Monter");
+    const fetchData=async ()=>{
+      try {
+        const response=await findEmploye(id)
+        setEmploye(response.data)
+      } catch (error) {
+          setError("Erreur lors de la recupération employé")
+      }
+    }
+    fetchData()
+    
+  },[id])
+
+  const [formData, setFormData] = useState({
+    matricule:"",
+    nom: "",
+    prenom: "",
+    tel: "",
+    sexe: "",
+    image: null,
+    poste: "",
+    dateN: "",
+    statut: "",
+    departement: "",
+    adresse: "",
+    date_embauche:""
+  });
+
+  
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setFormData({
+      ...formData,
+      image: file,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await createEmploye(formData);
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="bg-white dark:bg-gray-900 p-4 ">
+      <div className="mx-auto max-w-4xl lg:py-16">
+        <h2 className="mb-8 text-2xl font-bold text-gray-900 dark:text-white">
+          Modification un Employé
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1  gap-6">
+            {/* Left: Image Upload */}
+            <div className="flex flex-col items-center">
+              <div className="relative w-48 h-48 bg-gray-200 dark:bg-gray-700 overflow-hidden">
+                {formData.image && (
+                  <img
+                    src={URL.createObjectURL(formData.image)}
+                    alt="Employé"
+                    className="w-full h-full"
+                  />
+                )}
+              </div>
+              <label
+                htmlFor="image"
+                className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Image
+              </label>
+              <input
+                onChange={handleImageChange}
+                type="file"
+                id="image"
+                name="image"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                required
+              />
+            </div>
+
+            {/* Right: Form Inputs */}
+            <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+              {/* Informations personnelles */}
+             
+              <div className="w-full">
+                <label
+                  htmlFor="nom"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Matricule
+                </label>
+                <input
+                  onChange={handleChange}
+                  value={formData.matricule}
+                  name="matricule"
+                  id="matricule"
+                  type="text"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="nom"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Nom
+                </label>
+                <input
+                  onChange={handleChange}
+                  value={formData.nom}
+                  type="text"
+                  name="nom"
+                  id="nom"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="prenom"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Prénom
+                </label>
+                <input
+                  onChange={handleChange}
+                  value={formData.prenom}
+                  type="text"
+                  name="prenom"
+                  id="prenom"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="adresse"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Adresse
+                </label>
+                <input
+                  onChange={handleChange}
+                  value={formData.adresse}
+                  type="text"
+                  name="adresse"
+                  id="adresse"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="tel"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Téléphone
+                </label>
+                <input
+                  onChange={handleChange}
+                  value={formData.tel}
+                  type="number"
+                  name="tel"
+                  id="tel"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="sexe"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Sexe
+                </label>
+                <select
+                  onChange={handleChange}
+                  value={formData.sexe}
+                  name="sexe"
+                  id="sexe"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                >
+                  <option value="">-- Sélectionnez --</option>
+                  <option value="Homme">Homme</option>
+                  <option value="Femme">Femme</option>
+                </select>
+              </div>
+              <div>
+                <label
+                  htmlFor="dateN"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Date de naissance
+                </label>
+                <input
+                  onChange={handleChange}
+                  value={formData.dateN}
+                  type="date"
+                  name="dateN"
+                  id="dateN"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                />
+              </div>
+
+              {/* Informations professionnelles */}
+              <div>
+                <label
+                  htmlFor="poste"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Poste
+                </label>
+                <input
+                  onChange={handleChange}
+                  value={formData.poste}
+                  type="text"
+                  name="poste"
+                  id="poste"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="poste"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Departement
+                </label>
+                <input
+                  onChange={handleChange}
+                  value={formData.date_embauche}
+                  type="date"
+                  name="date_embauche"
+                  id="date_embauche"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="departement"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Département
+                </label>
+                <input
+                  onChange={handleChange}
+                  value={formData.departement}
+                  type="text"
+                  name="departement"
+                  id="departement"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="statut"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Statut
+                </label>
+                <select
+                  onChange={handleChange}
+                  value={formData.statut}
+                  name="statut"
+                  id="statut"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                >
+                  <option value="">-- Sélectionnez --</option>
+                  <option value="Permanent">Permanent</option>
+                  <option value="Contractuel">Contractuel</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          {/* Submit button */}
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg"
+            >
+              Ajouter
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
 
-export default Update
+export default EmployeUpdate;
